4cd99b26ba2a042aaedd4ea096b8881816c7851a
mudassir waseem <mudassir@mudassirs-MacBook-Air.local>
2022-09-14 12:44:32 +0500

    integrate stripe

---
+++ src/components/Checkout.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+import CardIcon from "../images/credit-card.svg";
+import ProductImage from "../images/product-image.jpg";
+
+import "../styles.css";
+
+let stripePromise;
+
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+  }
+
+  return stripePromise;
+};
+
+const Checkout = () => {
+  const [stripeError, setStripeError] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const item = {
+    price: "price_1Lha5jIuKVUFxOzsxCiNhCFG",
+    quantity: 3,
+  };
+
+  const checkoutOptions = {
+    lineItems: [item],
+    mode: "payment",
+    successUrl: `${window.location.origin}/success`,
+    cancelUrl: `${window.location.origin}/cancel`,
+  };
+
+  const redirectToCheckout = async () => {
+    setLoading(true);
+    console.log("redirectToCheckout");
+
+    const stripe = await getStripe();
+    const { error, success } = await stripe.redirectToCheckout(checkoutOptions);
+
+    if (error) setStripeError(error.message);
+    setLoading(false);
+  };
+  if (stripeError) alert(stripeError);
+
+  return (
+    <div className="checkout">
+      <h1 className="checkout-price">$299</h1>
+      <img
+        className="checkout-product-image"
+        src={ProductImage}
+        alt="Product"
+      />
+      <button
+        className="checkout-button"
+        onClick={redirectToCheckout}
+        disabled={isLoading}
+      >
+        <div className="grey-circle">
+          <div className="purple-circle">
+            <img className="icon" src={CardIcon} alt="credit-card-icon" />
+          </div>
+        </div>
+        <div className="text-container">
+          <p className="text">{isLoading ? "Loading..." : "Buy"}</p>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Checkout;
